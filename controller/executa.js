be18f9eade038cd45d71266d77ccde992cf3fafd
Giovanna Araujo <133662788+GigiAraujo@users.noreply.github.com>
2024-02-19 14:37:19 -0300

    Initial commit

---
+++ controller/executa.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Calculadora = require('../model/calculo');
+const resultado = express.Router();
+
+resultado.post('/', (req, res) => {
+    const {base, altura} = req.body;
+    const area = Calculadora.Area(parseFloat(base), parseFloat(altura));
+    res.redirect(`/resultado.html?area=${area}`);
+    //res.send(`a área é: ${area}`);
+});
+
+module.exports = resultado;
